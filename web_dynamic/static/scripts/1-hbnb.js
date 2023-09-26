39cbf557a1b5bb08f98d85895cdc5b2cfba4226a
Santiago Caritat <6317@holbertonstudents.com>
2023-09-26 09:16:09 -0700

    task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  let amenitees = {};
+
+  $('input[type="checkbox"]').change(function () {
+    let id = $(this).data('id');
+    let name = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      amenitees[id] = name;
+    } else {
+      delete amenitees[id];
+    }
+
+    let amenities = Object.values(amenitees).join(', ');
+    if (amenities.length > 30) {
+      amenities = amenities.substring(0, 30) + '...';
+    }
+    $('.amenities h4').text(amenities);
+  });
+});
